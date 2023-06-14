e10fc80402a18f6fef2f245a7dff3e5002423d9e
ARJUN KUMAR <102915876+ERROR404-WEB@users.noreply.github.com>
2023-06-14 12:10:45 +0530

    Removed Conflicts

--- frontend/src/components/Authentication/SignIn.js
+++ frontend/src/components/Authentication/SignIn.js
@@ -1,48 +1,48 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 import SignUp from './SignUp1';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const SignIn = () => {
-    const [showSignUp, setShowSignUp] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
 
-    if (showSignUp) {
-        return <SignUp />;
-      }    
+  if (showSignUp) {
+    return <SignUp />;
+  }
   return (
     <Form style={formstyle}>
-        <div className='newDivision' style={divstyle}>
-      <Stack gap={2} className="col-md-3 mx-auto">
+      <div className='newDivision' style={divstyle}>
+        <Stack gap={2} className="col-md-3 mx-auto">
 
-      <Form.Floating className="mb-3">
-        <Form.Control
-          id="floatingInputCustom"
-          type="number"
-          placeholder="name@example.com"
-        />
-        <label htmlFor="floatingInputCustom">Mobile Number</label>
-      </Form.Floating>
+          <Form.Floating className="mb-3">
+            <Form.Control
+              id="floatingInputCustom"
+              type="number"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="floatingInputCustom">Mobile Number</label>
+          </Form.Floating>
 
-      <Form.Floating className="mb-3">
-        <Form.Control
-          id="floatingPasswordCustom"
-          type="password"
-          placeholder="Password"
-        />
-        <label htmlFor="floatingPasswordCustom">Password</label>
-      </Form.Floating>
+          <Form.Floating className="mb-3">
+            <Form.Control
+              id="floatingPasswordCustom"
+              type="password"
+              placeholder="Password"
+            />
+            <label htmlFor="floatingPasswordCustom">Password</label>
+          </Form.Floating>
 
-      <Button variant="primary" type="submit" className='mb-3'>
-        Login
-      </Button>
+          <Button variant="primary" type="submit" className='mb-3'>
+            Login
+          </Button>
 
-      <center><p>
-        New to FarmBank? &nbsp;
-        <a style={anchor} onClick={()=> {setShowSignUp(true)}}>Sign Up</a>
-      </p></center>
+          <center><p>
+            New to FarmBank? &nbsp;
+            <a style={anchor} onClick={() => { setShowSignUp(true) }}>Sign Up</a>
+          </p></center>
 
-      </Stack> </div>
+        </Stack> </div>
     </Form>
   )
 }
@@ -62,8 +62,8 @@ const divstyle = {
   paddingBottom: '20px',
 }
 const anchor = {
-    cursor: 'pointer',
-    color: 'blue purple',
+  cursor: 'pointer',
+  color: 'blue purple',
 }
 
 export default SignIn
