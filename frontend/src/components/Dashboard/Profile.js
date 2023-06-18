412c356603a01c2502ecd9bf9542978a8c5de0e9
ARJUN KUMAR <102915876+ERROR404-WEB@users.noreply.github.com>
2023-06-18 22:46:30 +0530

    removed axios link from profile.js

--- frontend/src/components/Dashboard/Profile.js
+++ frontend/src/components/Dashboard/Profile.js
@@ -10,8 +10,6 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
-import axios from 'axios';
-
 
 import { useNavigate } from 'react-router-dom';
 
