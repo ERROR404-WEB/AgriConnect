adee72b1f3aa834d40aad1ed0e09112b316e1bc5
Doddi Avinash <avinashdoddi2001@gmail.com>
2023-06-21 03:13:39 +0530

    updated the post upload form.

--- frontend/src/components/Posts/postItem.js
+++ frontend/src/components/Posts/postItem.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './Posts.scss';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 function PostModal(props) {
+  const handleClose = () => props.onHide();
+  const checkData = async () => {
+    props.setTyping(props.typing.length <= 0? " " : props.typing);
+  }
+  const saveChanges = async () => {
+    // for the backend part...
+  }
   return (
     <Modal
       {...props} // array containing all the props passed to the component. (spread operator)
@@ -17,13 +25,24 @@ function PostModal(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <input className='modal-input' 
-        placeholder='What do you want to talk about?'
-        onChange={(event) => props.setTyping(event.target.value)}
-        value = {props.typing}/>
+        <Form>
+          <input type = "text" className='modal-input' 
+          placeholder='What do you want to talk about?'
+          onChange={(event) => props.setTyping(event.target.value)}
+          value = {props.typing}/>
+
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label className = 'modal-file-input'>Upload a File</Form.Label>
+                <Form.Control
+                type="file"   autoFocus
+                onChange={checkData}
+                />
+          </Form.Group>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide} disabled={props.typing.length > 0 ? false : true}>Post</Button>
+      <Button variant="secondary" onClick={handleClose}> Close </Button>
+    <Button onClick={props.onHide} disabled={props.typing.length > 0 ? false : true}>Post</Button> {/* props.onHide to be replaced by saveChanges after the functionality*/}
       </Modal.Footer>
     </Modal>
   );
--- frontend/src/components/Posts/posts.js
+++ frontend/src/components/Posts/posts.js
@@ -5,6 +5,12 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import PostItems from './postItem';
+import videoIcon from './images/video-icon.png';
+import userIcon from './images/user.svg';
+import photoIcon from './images/photo-icon.svg';
+import articleIcon from './images/event-icon.png';
+import Form from 'react-bootstrap/Form';
+
 
 
 export default function Posts() {
@@ -24,7 +30,24 @@ function PostsAndDetails() {
       <Row>
         <Col xs={9} md={9} lg={9} xxl={9}>
           <div className='post-prompt'>
-            <button className='post-button' onClick={() => setModalShow(true)}>Start a Post</button>
+            <div className='post-prompt-text'>
+              <img src = {userIcon} alt = "user-image" className="user-icon"/>
+              <button className='post-button' onClick={() => setModalShow(true)}>Start a Post</button>
+            </div>
+            <div className='post-icons'>
+              <button className='icon-grp' onClick={() => setModalShow(true)}>
+                <img src = {photoIcon} alt = "photo-icon" className="icon"/>
+                <span>Photo</span>
+              </button>
+              <button className='icon-grp' onClick={() => setModalShow(true)}>
+                <img src = {videoIcon} alt = "video-icon" className="icon"/>
+                <span>Video</span>
+              </button>
+              <button className='icon-grp' onClick={() => setModalShow(true)}>
+                <img src = {articleIcon} alt = "article-icon" className="icon"/>
+                <span>Article</span>
+              </button>
+            </div>
             <PostItems modalShow={modalShow} setModalShow={setModalShow} typing={typing} setTyping={setTyping}/>
           </div>
         </Col>
